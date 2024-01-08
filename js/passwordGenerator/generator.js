c7f9fa27078d6b25d2a3d4e451cd97dfb32351bb
ITQ <itq.dev@ya.ru>
2024-01-08 21:22:00 +0300

    Refactored HTML and fixed issue with incorrect output on mobile(probably)

--- js/passwordGenerator/generator.js
+++ js/passwordGenerator/generator.js
@@ -26,7 +26,7 @@ function getRandomNumber() {
 }
 
 function getRandomSymbol() {
-  const symbols = "`~!@#$%^&*()_-+=|{}[]?/<>,.':;";
+  const symbols = "!@#$%^&*()_+-={}[];<>:";
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
